6384c55c497a70b629618a08c0aa17646865a2d8
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-29 11:55:23 +0530

    Layout for recyclerView created

--- app/src/main/java/com/example/gohan/volsonar/display_list.java
+++ app/src/main/java/com/example/gohan/volsonar/display_list.java
@@ -2,12 +2,21 @@ package com.example.gohan.volsonar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class display_list extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_list);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
     }
 }
