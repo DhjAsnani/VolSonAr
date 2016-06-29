1196aa6811ddeca923ebcae9e8a8fce0390f71ee
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-29 12:28:01 +0530

    Singleton implemented

--- app/src/main/java/com/example/gohan/volsonar/Singleton.java
+++ app/src/main/java/com/example/gohan/volsonar/Singleton.java
@@ -1,7 +1,42 @@
 package com.example.gohan.volsonar;
 
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 /**
  * Created by Freeware Sys on 29-Jun-16.
  */
 public class Singleton {
+    private static Singleton mInstance;
+    private RequestQueue requestQueue;
+    private static Context nCtx;
+
+    private Singleton (Context context)
+    {
+        nCtx = context;
+        requestQueue = getRequestQue();
+    }
+    public RequestQueue getRequestQue()
+    {
+        if(requestQueue==null)
+        {
+            requestQueue = Volley.newRequestQueue(nCtx.getApplicationContext());
+        }
+        return requestQueue;
+    }
+    public static synchronized Singleton getInstance(Context context)
+    {
+        if(mInstance == null)
+        {
+            mInstance = new Singleton(context);
+        }
+        return mInstance;
+    }
+    public <T> void addToRequestQ(Request<T> request)
+    {
+        requestQueue.add(request);
+    }
 }
